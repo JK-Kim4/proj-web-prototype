a45b0e2bd4634e200b75dbb277e331e416679b3c
jongwankim <jongbell4@gmail.com>
2023-05-31 17:41:06 +0900

    feat : (Memory)사용자 정보 저장 및 조회

--- src/main/java/com/jw/webprototype/presentation/MemoryUserRepository.java
+++ src/main/java/com/jw/webprototype/presentation/MemoryUserRepository.java
@@ -2,14 +2,17 @@ package com.jw.webprototype.presentation;
 
 import com.jw.webprototype.controller.dto.UserDto;
 import com.jw.webprototype.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class MemoryUserRepository implements UserRepository{
 
-    private List<User> userList = new ArrayList<>();
+    private final Logger logger = LoggerFactory.getLogger(MemoryUserRepository.class);
+
+    private static List<User> userList = new ArrayList<>();
 
     @Override
     public Long save(UserDto dto) {
@@ -17,11 +20,15 @@ public class MemoryUserRepository implements UserRepository{
         User user = new User(dto);
         userList.add(user);
 
+        logger.debug("save user result 1 = {}", userList.size());
+        logger.debug("save user result 2 = {}", user.getSeq());
+
+
         return user.getSeq();
     }
 
     @Override
     public User findbyUserId(String id) {
-        return userList.stream().filter(u -> u.equals(id)).findAny().get();
+        return userList.stream().filter(u -> u.getId().equals(id)).findAny().get();
     }
 }
