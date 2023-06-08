b4781954e887f5195f43b3366d438143bfdd4873
jongwankim <jongbell4@gmail.com>
2023-06-08 14:43:38 +0900

    feat: 파일 저장시 Blob Encrypting 후 저장 테스트

---
+++ src/test/java/com/jw/webprototype/FileUtilTest.java
@@ -0,0 +1,21 @@
+package com.jw.webprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FileUtilTest {
+
+    /*File Blob Data Encrypting As AES-256*/
+    @Test
+    public void file_blob_enc_test(){
+
+    }
+
+    /*File Blob Data Decrypting As AES-256*/
+    @Test
+    public void file_blob_dec_test(){
+
+    }
+
+}
