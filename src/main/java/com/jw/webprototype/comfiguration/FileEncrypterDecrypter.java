fd25e61335e5988cfd2b2e40b3566d8c82a7b92b
jongwankim <jongbell4@gmail.com>
2023-06-08 14:58:30 +0900

    feat: 기타수정

--- src/main/java/com/jw/webprototype/comfiguration/FileEncrypterDecrypter.java
+++ src/main/java/com/jw/webprototype/comfiguration/FileEncrypterDecrypter.java
@@ -2,30 +2,43 @@ package com.jw.webprototype.comfiguration;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 public class FileEncrypterDecrypter {
 
-    FileEncrypterDecrypter(SecretKey secretKey, String transformation) throws NoSuchPaddingException, NoSuchAlgorithmException {
+    FileEncrypterDecrypter(SecretKey secretKey, String transformation) {
         this.secretKey = secretKey;
-        this.cipher = Cipher.getInstance(transformation);
+        try {
+            this.cipher = Cipher.getInstance(transformation);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private SecretKey secretKey;
     private Cipher cipher;
 
     void encrypt(String content, String fileName) {
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
         byte[] iv = cipher.getIV();
 
         try (FileOutputStream fileOut = new FileOutputStream(fileName);
              CipherOutputStream cipherOut = new CipherOutputStream(fileOut, cipher)) {
             fileOut.write(iv);
             cipherOut.write(content.getBytes());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -51,6 +64,14 @@ public class FileEncrypterDecrypter {
                 content = sb.toString();
             }
 
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidAlgorithmParameterException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
         }
         return content;
     }
