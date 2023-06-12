a3bac11cb99e333ff84cd27242c00901f6e90e41
jongwan <jongbell4@gmail.com>
2023-06-12 16:14:00 +0900

    feat: ResponseEntity redirect 처리

--- src/main/java/com/jw/webprototype/controller/ItemController.java
+++ src/main/java/com/jw/webprototype/controller/ItemController.java
@@ -6,6 +6,9 @@ import com.jw.webprototype.domain.Item;
 import com.jw.webprototype.service.ItemService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @Controller
@@ -60,14 +64,15 @@ public class ItemController {
     }
 
     @PostMapping("/insert")
-    @ResponseBody
-    public Long insertMethod(
+    public ResponseEntity<Object> insertMethod(
             ItemSaveDto dto,
             MultipartFile file) throws IOException {
 
+        HttpHeaders headers = new HttpHeaders();
 
-
-        return itemService.save(dto, file);
+        headers.setLocation(URI.create("/item/list"));
+        itemService.save(dto, file);
+        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 
 }
